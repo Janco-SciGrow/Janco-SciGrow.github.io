325108d95e347a10af6b05b8bb9f78a132e78179
Janco-SciGrow <janco@scigrow.tech>
2023-08-24 18:39:40 +0200

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "497a28142b0f8d56aeb424952558fb74",
-"/": "497a28142b0f8d56aeb424952558fb74",
+const RESOURCES = {"index.html": "5fbec9346d38a50ec86437382965b5e6",
+"/": "5fbec9346d38a50ec86437382965b5e6",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "canvaskit/chromium/canvaskit.wasm": "393ec8fb05d94036734f8104fa550a67",
 "canvaskit/chromium/canvaskit.js": "ffb2bb6484d5689d91f393b60664d530",
@@ -14,7 +14,7 @@ const RESOURCES = {"index.html": "497a28142b0f8d56aeb424952558fb74",
 "canvaskit/skwasm.js": "95f16c6690f955a45b2317496983dbe9",
 "canvaskit/canvaskit.js": "5caccb235fad20e9b72ea6da5a0094e6",
 "version.json": "d37a8444b248391960d8f9f0f0593a3f",
-"main.dart.js": "70062b2f126095f9c7f2549d17a4b9f6",
+"main.dart.js": "53d2dfe3fbab60f647c3d88ad261c06b",
 "assets/NOTICES": "29d5b5e30d9c47ea4f2e4e238188e468",
 "assets/fonts/MaterialIcons-Regular.otf": "fefafece3c058dd86da39a581a35e1dd",
 "assets/AssetManifest.json": "b122e4c20475dcc136e5e259e9adf1ad",
